e2a7c3cc3ae2a654c45d77464c6d3b24ffe95561
ThagillaRakesh <thagillarakesh@gmail.com>
2026-03-25 16:26:15 +0530

    Update CalculatorApplication.java

--- Desktop/DevOps/week5/calculator/src/main/java/com/example/calculator/CalculatorApplication.java
+++ Desktop/DevOps/week5/calculator/src/main/java/com/example/calculator/CalculatorApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
-	public int add(int a, int b){
-	  return a+b;
-	}
-	  
-	  public int subtract(int a, int b){
-	    return a-b;
-	  }
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
 
+    public int add(int a, int b){
+        return a + b;
+    }
+      
+    public int subtract(int a, int b){
+        return a - b;
+    }
 }
