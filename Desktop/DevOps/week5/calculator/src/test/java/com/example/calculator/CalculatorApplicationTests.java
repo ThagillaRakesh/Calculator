671a112f809ce30583001757b9e39e7587267352
ThagillaRakesh <thagillarakesh@gmail.com>
2026-03-25 16:24:44 +0530

    Update CalculatorApplicationTests.java

--- Desktop/DevOps/week5/calculator/src/test/java/com/example/calculator/CalculatorApplicationTests.java
+++ Desktop/DevOps/week5/calculator/src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -2,19 +2,20 @@ package com.example.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
-	@Test
-	public void testAdd(){
-	 CalculatorApplication c=new CalculatorApplication();
-	 assertEquals(5,c.add(2,3));
-	 }
-	 
-	 public void testSubtract(){
-	 CalculatorApplication c=new CalculatorApplication();
-	 assertEquals(5,c.subtract(2,3));
-	 }
+    @Test
+    public void testAdd() {
+        CalculatorApplication c = new CalculatorApplication();
+        assertEquals(5, c.add(2, 3));
+    }
 
+    @Test
+    public void testSubtract() {
+        CalculatorApplication c = new CalculatorApplication();
+        assertEquals(-1, c.subtract(2, 3));
+    }
 }
